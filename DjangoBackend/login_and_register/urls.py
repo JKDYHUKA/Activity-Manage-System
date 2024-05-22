5ffe1f3276eedf96d9584cff0d0e6ff5d62a354d
JKDYHUKA <1746935479@qq.com>
2024-05-22 16:33:58 +0800

    modify urls.py and complete register part

--- DjangoBackend/login_and_register/urls.py
+++ DjangoBackend/login_and_register/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('api/get_current_time/', views.get_current_time, name='test'),
+    path('api/get_captcha/', views.verify, name='captcha'),
+    path('api/submit_register_form', views.register, name='register')
 ]
