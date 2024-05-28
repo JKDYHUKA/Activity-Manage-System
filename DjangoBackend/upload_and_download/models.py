3a3d9794b94665218d48d0036fbbdd6740282749
PJX28 <u202142421@xs.ustb.edu.cn>
2024-05-28 23:08:05 +0800

    connect FileInfo with creatActivity

--- DjangoBackend/upload_and_download/models.py
+++ DjangoBackend/upload_and_download/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.utils import timezone
+from activities_organization.models import CreatActivity
 
 
 class FileInfo(models.Model):
-    active_id = models.IntegerField(max_length=50)
+    active = models.ForeignKey(CreatActivity, on_delete=models.CASCADE)
     file_name = models.CharField(max_length=500)
     file_size = models.DecimalField(max_digits=10, decimal_places=0)
     file_path = models.CharField(max_length=500)
